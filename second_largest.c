958b087953fcd514ce6743f8454f37ce2c0e4166
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2023-04-04 13:11:12 +0530

    Create second_largest.c

---
+++ second_largest.c
@@ -0,0 +1,35 @@
+/* C program to print second largest element of the array*/
+//Save it as SecondLargestElementArray.c
+#include<stdio.h>
+#include<limits.h>
+int main(){
+    int i,n;
+    printf("Enter the size of array : ");
+    scanf("%d",&n);
+    //Declaring array
+    int arr[n];
+    printf("Enter the elements of the array : ");
+    for(i=0;i<n;i++) {
+        scanf("%d",&arr[i]);
+    }
+    
+    //Declaring maximum element
+    int maximum = INT_MIN;
+    //Comparing with each element and find maximum element
+    for(i=0;i<n;i++) {
+        if(arr[i] > maximum) {
+            maximum = arr[i];
+        }
+    }
+    printf("The maximum value is : %d", maximum);
+    int second_max = INT_MIN;
+    //Finding Second largest element.
+    //Comparing with each element and also checking it is not equal to max
+    
+    for(i=0;i<n;i++) {
+        if(arr[i] > second_max && arr[i]!=maximum) {
+            second_max = arr[i];
+        }
+    }
+    printf("\nThe second maximum value is : %d", second_max);
+}
