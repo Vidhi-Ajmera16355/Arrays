55d9bc44f086f31ee31f3b482b65ab148d14afdd
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2023-04-04 17:54:10 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>    
+     
+int main()    
+{    
+    int n;
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    //Initialize array     
+    int arr[n];     
+    //Calculate length of array    
+    int length = sizeof(arr)/sizeof(arr[0]);    
+    
+    //Loop through the array by incrementing value of i     
+    for (int i = 0; i < length; i++) { 
+        printf("Enter element number %d\n",i+1);
+        scanf("%d", &arr[i]);     
+    }      
+    printf("Elements of the given array are ");
+    for (int i=0;i< length;i++){
+        if (arr[i]<0)
+        printf("%d ",arr[i]);
+    }
+    
+    return 0;    
+}   
