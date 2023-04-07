ab3ec1e3c0c576e12d022b6bee06c4ce6bba78ed
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2023-04-07 09:23:03 +0530

    Update second_largest.c

--- second_largest.c
+++ second_largest.c
@@ -33,3 +33,31 @@ int main(){
     }
     printf("\nThe second maximum value is : %d", second_max);
 }
+
+
+----------------------------------------------------------------OR------------------------------------------------------------------------
+    #include<stdio.h>
+
+int main(){
+    int i,n;
+    printf("Enter the size of array : ");
+    scanf("%d",&n);
+
+    int arr[n];
+    printf("Enter the elements of the array : ");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int max1,max2 = arr[0];
+    
+    for(i=0;i<n;i++) {
+        if(arr[i] > max1) {
+            max2=max1;
+            max1=arr[i];
+        }
+        else if (arr[i]<max1 && arr[i]>max2){
+            max2=arr[i];
+        }
+}
+printf("Second max is : %d ",max2);
+}
